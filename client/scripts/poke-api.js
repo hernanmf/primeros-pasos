87a81e640af30ca04b0c39bd8cf6af020debb1f2
hernanmf <hernanmartinezf@gmail.com>
2023-04-09 18:26:30 -0300

    funciona el consumo de la api, falta volacarlo al html

--- client/scripts/poke-api.js
+++ client/scripts/poke-api.js
@@ -1,12 +1,40 @@
-function obtenerPokemon() {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+class Pokemon {
+  constructor(name, order, height, weight, imageUrl) {
+    this.name = name;
+    this.order = order;
+    this.height = height;
+    this.weight = weight;
+    this.imageUrl = imageUrl;
+  }
+}
+
+let listaDePokemones = [];
+
+function obtenerPokemones() {
+  let urlDeLista = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0';
+  fetch(urlDeLista)
     .then((response) => response.json())
     .then((data) => {
       data.results.forEach((pokemon) => {
-        console.log(pokemon.name);
+        obtenerPokemon(pokemon.url);
       });
     });
 }
 
-// Llamar a la función para obtener los pokemones
-obtenerPokemon();
+function obtenerPokemon(urlDetallado) {
+  fetch(urlDetallado)
+    .then((response) => response.json())
+    .then((data) => {
+      let nuevoPokemon = new Pokemon(
+        data.name,
+        data.order,
+        data.height,
+        data.weight,
+        data.sprites.other.home.front_default,
+      );
+      listaDePokemones.push(nuevoPokemon);
+    });
+}
+
+obtenerPokemones();
+console.log(listaDePokemones);
